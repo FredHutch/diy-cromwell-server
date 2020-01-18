77bd8b7a923ad57d8921daf06e2137311c9bd4f0
Amy Paguirigan <amypaguirigan@gmail.com>
2020-01-18 07:54:21 -0800

    shorten default wall time

--- config/cromwellParams.sh
+++ config/cromwellParams.sh
@@ -1,18 +1,18 @@
 ## Where do you want the working directory to be for Cromwell?  
 ### Suggestion: /fh/scratch/delete90/pilastname_f/cromwell-executions
-SCRATCHPATH=/fh/scratch/delete90/pilastname_f/cromwell-executions
+SCRATCHPATH=/fh/scratch/delete90/pilastname_f/crowell-executions
 
 ## Where do you want logs about individual workflows (not jobs) to be written?
-### Suggestion: /fh/fast/pilastname_f/cromwell/cromwell-workflow-logs
-WORKFLOWLOGDIR=/fh/fast/pilastname_f/cromwell/cromwell-workflow-logs
+### Suggestion: /fh/fast/pilastname_f/cromwell/workflow-logs
+WORKFLOWLOGDIR=/fh/fast/pilastname_f/cromwell/workflow-logs
 
 ## Where do you want final output files specified by workflows to be copied for your subsequent use?
-### Suggestion: /fh/fast/pilastname_f/cromwell/cromwell-outputs
-WORKFLOWOUTPUTSDIR=/fh/fast/pilastname_f/cromwell/cromwell-outputs
+### Suggestion: /fh/fast/pilastname_f/cromwell/outputs
+WORKFLOWOUTPUTSDIR=/fh/fast/pilastname_f/cromwell/outputs
 
 ## Where do you want individual task-level logs to be written after a workflow is successful?
-### Suggestion: /fh/fast/pilastname_f/cromwell/cromwell-outputs
-WORKFLOWCALLLOGSDIR=/fh/fast/pilastname_f/cromwell/cromwell-outputs
+### Suggestion: /fh/fast/pilastname_f/cromwell/outputs
+WORKFLOWCALLLOGSDIR=/fh/fast/pilastname_f/cromwell/outputs
 
 ## Where is your configuration file?
 ### Suggestion: /fh/fast/pilastname_f/cromwell/config
